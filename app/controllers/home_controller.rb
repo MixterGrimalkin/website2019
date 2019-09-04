164e0a621a3b91a22896d4e2b001e91ae4e14b21
grimalkin <loki@amarantha.net>
2019-09-04 17:45:41 +0200

    Integrate MailGun for contact form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
-  def gridtest
 
+  def contact_me
+    data = {}
+    data[:from] = "#{params[:name]} <#{params[:email]}>"
+    data[:reply_to] = params[:email]
+    data[:to] = 'barrimason@amarantha.net'
+    data[:subject] = 'Message via website'
+    data[:text] = params[:message]
+    RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}@api.eu.mailgun.net/v3/#{ENV['MAILGUN_DOMAIN']}/messages", data
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get '/gt' => 'home#gridtest'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  post '/contact_me' => 'home#contact_me'
+
 end
