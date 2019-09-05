2764d29100e136e8b2f39fbd7cbe9876276575ac
grimalkin <loki@amarantha.net>
2019-09-05 21:19:17 +0200

    Data upload script reset ID sequence

--- tools/upload_projects.rb
+++ tools/upload_projects.rb
@@ -11,18 +11,19 @@ puts
 mode = STDIN.getch.to_i
 
 if [1,2].include? mode
-  puts 'WARNING: This will overwrite the target database!'
+  puts 'WARNING: This will overwrite Projects in the target database!'
   puts 'Are you sure? (y/n)'
   puts
   confirm = STDIN.getch.upcase
   if confirm == 'Y'
     puts 'Fetching database connection string....'
     target_db = `heroku config:get DATABASE_URL -a barrimason#{mode==1 ? '-staging' : ''}`.strip
-    puts target_db
     puts 'Deleting target....'
     puts `psql #{target_db} -c 'delete from projects'`
     puts 'Uploading local data....'
-    puts `psql -d website2019_development -c "copy (select * from projects) to STDOUT" | psql #{target_db} -c "copy projects from STDIN"`
+    puts `psql -d website2019_development -c 'copy (select * from projects) to STDOUT' | psql #{target_db} -c 'copy projects from STDIN'`
+    puts 'Updating ID sequence....'
+    puts `psql #{target_db} -c "select setval('projects_id_seq', max(id)) from projects"`
     puts 'Done.'
   end
 end
