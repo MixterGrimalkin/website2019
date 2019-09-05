428c96858cc0f0a2e074bd7ecda1cf74dcf38dc9
grimalkin <loki@amarantha.net>
2019-09-05 20:57:06 +0200

    Create and delete projects Fade out notices Image viewer

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,16 +3,34 @@ class AdminController < ApplicationController
   before_action :require_admin!
 
   def index
-    @projects = Project.all.order(created_at: :desc)
+    @projects = Project.all.order(title: :asc)
+  end
+
+  def new_project
+    @project = Project.new
+    render :edit_project
   end
 
   def edit_project
     begin
       @project = Project.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "Project #{params[:id]} not found!"
+      flash[:alert] = "Project '#{params[:id]}' not found!"
       redirect_to '/'
     end
+  end
+
+  def create_project
+    project = Project.create!(
+        {
+            title: params[:title],
+            description: params[:description],
+            content: params[:content],
+            image: params[:image]
+        }
+    )
+    flash[:notice] = 'Project created'
+    redirect_to "/project/#{project.id}/edit"
   end
 
   def update_project
@@ -29,6 +47,18 @@ class AdminController < ApplicationController
       flash[:alert] = "Project #{params[:id]} not found!"
       redirect_to '/admin'
     rescue => e
+      flash[:alert] = "Project #{params[:id]} not found!"
+      redirect_back fallback_location: '/admin'
+    end
+  end
+
+  def delete_project
+    begin
+      project = Project.find(params[:id])
+      project.destroy
+      flash[:notice] = 'Project deleted'
+      redirect_to '/admin'
+    rescue
       flash[:alert] = "Project #{params[:id]} not found!"
       redirect_back fallback_location: '/admin'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   get '/project/:id' => 'home#project'
 
   get '/admin' => 'admin#index'
+
+  get '/new_project' => 'admin#new_project'
+  post '/create_project' => 'admin#create_project'
+
   get '/project/:id/edit' => 'admin#edit_project'
+  delete '/project/:id/delete' => 'admin#delete_project'
   post '/update_project/:id' => 'admin#update_project'
 
   post '/contact_me' => 'home#contact_me'
